6faeb7325d620468a64cd2c6b5804e3c0c350320
ZhouJianuo <104631604+ZhouJianuo@users.noreply.github.com>
2023-12-10 21:43:58 -0500

    Update txt.js

--- public/javascripts/txt.js
+++ public/javascripts/txt.js
@@ -22,8 +22,8 @@ var txt = {
         },
     ],
     "About": {
-        "CH": "玉衡杯数据库依托玉衡杯网站建立，数据由<b>妮可少女</b>提供",
-        "EN": "HomDGCat Database (formerly Yuheng Cup Database) is built upon the site of Yuheng Cup, a Keqing-centered speedrunning contest held by Chinese Keqing players. The database is maintained by <b>HomDGCat</b>."
+        "CH": "玉衡杯数据库部分依托玉衡杯网站建立，数据由<b>妮可少女</b>提供",
+        "EN": "HomDGCat Database (formerly Yuheng Cup Database) is built partly upon the site of Yuheng Cup, a Keqing-centered speedrunning contest held by Chinese Keqing players. The database is maintained by <b>HomDGCat</b>."
     },
     "About_2": {
         "CH": "若发现数据有误或有建议<br>↓<br><a href='https://space.bilibili.com/3537104994831140' target='_blank'>(bilibili)</a> <a href='https://www.reddit.com/user/homdgcat/' target='_blank'>(Reddit)</a> <a href='https://www.twitter.com/homdgcat_' target='_blank'>(Twitter)</a>",
