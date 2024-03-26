9395a414e04bdfe4482e362b631fb2dadf53caa5
ZhouJianuo <104631604+ZhouJianuo@users.noreply.github.com>
2024-03-25 23:27:25 -0400

    1

--- public/data/PT/Chaos_1.js
+++ public/data/PT/Chaos_1.js
@@ -1130,8 +1130,8 @@ var _chaos = [
                     "Win with at least 20 Cycle(s) left",
                     "No more than 0 downed character(s)"
                 ],
-                "HPSingle": 772616,
-                "HPMulti": 523737,
+                "HPSingle": 470944,
+                "HPMulti": 272343,
                 "Upper": [
                     {
                         "Level": 95,
@@ -6164,7 +6164,7 @@ var _chaoshp = {
             433842,
             565636,
             477648,
-            772616
+            470944
         ],
         "Multi": [
             256250,
@@ -6173,7 +6173,7 @@ var _chaoshp = {
             317880,
             410610,
             255584,
-            523737
+            272343
         ],
         "Index": {
             "1009": 0,
